5ed86de9b9e49b9c44fd5c8fadfbf87cabd9facf
{G} gurraoptimus <63198393+gurraoptimus@users.noreply.github.com>
2023-11-20 20:43:15 +0100

    script

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-const container = document.querySelector('.');
+const container = document.querySelector('.container');
+const search = document.querySelector('.search-box button');
+const Weatherbox = document.querySelector('.Weather-box');
+const Weatherdetails = document.querySelector('.Weather-details');
+
+search.addEventListener("click", ()=>{
+    const APIKey = ""
+});
