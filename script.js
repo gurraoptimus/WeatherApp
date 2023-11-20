42e641b8459a7f96b2046635b8bd919553b2bd81
{G} gurraoptimus <63198393+gurraoptimus@users.noreply.github.com>
2023-11-20 21:02:56 +0100

    script

--- script.js
+++ script.js
@@ -4,5 +4,13 @@ const Weatherbox = document.querySelector('.Weather-box');
 const Weatherdetails = document.querySelector('.Weather-details');
 
 search.addEventListener("click", ()=>{
-    const APIKey = ""
+    const APIKey = "";
+    const city = document.querySelector('.search-box input').value;
+
+    if (city == '')
+        return;
+    
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid={APIKey}`).then(response=> response.json()).then(json=>{
+        const image=document.querySelector()
+    });
 });
